732a53b0f7133fcad15baa34135a6e7f24ca2507
Cao Dong <caodong@jkinvest.cn>
2019-05-10 11:24:42 +0800

    init

---
+++ Core Animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Core Animation
+//
+//  Created by CaoDong on 2019/5/10.
+//  Copyright © 2019 CaoDong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Core Animation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Core Animation
+//
+//  Created by CaoDong on 2019/5/10.
+//  Copyright © 2019 CaoDong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
